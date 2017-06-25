bfaee82d144e205314918eee742c908e13dd9de1
islam-Ellithy <islam.emmo@gmail.com>
2017-06-25 16:03:33 +0200

    movie app version 11

--- MovieApp-master/app/src/main/java/com/example/islam/movieapp/OnTaskCompleted.java
+++
@@ -1,9 +0,0 @@
-package com.example.islam.movieapp;
-
-/**
- * Created by islam on 12/10/2016.
- */
-public interface OnTaskCompleted{
-    void onTaskCompleted(Movie movie);
-    Movie getMovie();
-}
